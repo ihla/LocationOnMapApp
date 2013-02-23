1e982847840e5f36c490057222f87c89465080f0
ihla <lubos.ilcik@gmail.com>
2013-02-23 22:40:56 +0100

    - MapView working finally

--- MyLocationOnMap/src/com/joyatwork/mylocationonmap/ShowMapActivity.java
+++ MyLocationOnMap/src/com/joyatwork/mylocationonmap/ShowMapActivity.java
@@ -3,66 +3,92 @@ package com.joyatwork.mylocationonmap;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 
 public class ShowMapActivity extends Activity {
-	static final LatLng BLAVA_CASTLE = new LatLng(48.14174, 17.099971);
-	static final LatLng SENICA_SWEAT_HOME = new LatLng(48.678967, 17.365417);
+	
+	private MapView mapView;
 	private GoogleMap map;
 	
+	static final LatLng SENICA_SWEAT_HOME = new LatLng(48.678967, 17.365417);
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_show_map);
 		
-		
-		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		mapView = (MapView) findViewById(R.id.map);
+		mapView.onCreate(savedInstanceState);
 		
 		/*
-		MapView mapView = (MapView) findViewById(R.id.map);
-		mapView.onCreate(savedInstanceState);
-
+		 * this init is necessary here!
+		 * It must be called because some classes such as BitmapDescriptorFactory and CameraUpdateFactory need to be initialized.
+		 */ 
 		try {
 		     MapsInitializer.initialize(this);
 		 } catch (GooglePlayServicesNotAvailableException e) {
 		     e.printStackTrace();
 		 }
 
-		map = mapView.getMap();
-		*/
+		setUpMapIfNeeded();		
+	}
+
+    private void setUpMapIfNeeded() {
+        if (map == null) {
+            map = ((MapView) findViewById(R.id.map)).getMap();
+            if (map != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+		map.addMarker(new MarkerOptions()
+		.position(SENICA_SWEAT_HOME)
+		.title("Senica, Robotnicka ul.")
+		.snippet("Sweat Home")
+		.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_maps_indicator_current_position))
+		);
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(SENICA_SWEAT_HOME, 15));
+		map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+    }
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mapView.onResume();
 		
-		/* */
-		if (map != null) {
-			map.addMarker(new MarkerOptions()
-				.position(SENICA_SWEAT_HOME)
-				.title("Senica, Robotnicka ul.")
-				.snippet("Sweat Home")
-				.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_maps_indicator_current_position))
-				);
-			map.moveCamera(CameraUpdateFactory.newLatLngZoom(SENICA_SWEAT_HOME, 15));
-			map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
-		}
-		else {
-			// display warning or something
-		}
-		/* */
-			
+		setUpMapIfNeeded();
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_show_map, menu);
-		return true;
+	protected void onPause() {
+		mapView.onPause();
+		super.onPause();
+	}
+
+	@Override
+	protected void onDestroy() {
+		mapView.onDestroy();
+		super.onDestroy();
+	}
+
+	@Override
+	public void onLowMemory() {
+		super.onLowMemory();
+		mapView.onLowMemory();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		mapView.onSaveInstanceState(outState);
 	}
 
 }
